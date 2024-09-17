ea30ee71e73d4735ba309165da9521951df88ceb
gwl <root@Laptop-109>
2024-09-17 16:24:27 +0530

    final 2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 3000
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World New 1 \n')
+  response.end('Hello World New 2 \n')
 })
 
 server.listen(port)
